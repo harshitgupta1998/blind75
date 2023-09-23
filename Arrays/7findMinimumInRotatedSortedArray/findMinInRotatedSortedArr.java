4239426f7560d40fd13d6133394bcfb91f6c1e8c
Sugapriyan-P-K <sugapriyan2003@gmail.com>
2023-09-23 20:27:25 +0530

    updated

--- Arrays/7findMinimumInRotatedSortedArray/findMinInRotatedSortedArr.java
+++ Arrays/7findMinimumInRotatedSortedArray/findMinInRotatedSortedArr.java
@@ -2,21 +2,20 @@ public class findMinInRotatedSortedArr {
     private static int findIt(int[] nums) {
         int res = nums[0];
         int l = 0,
-            r = nums.length - 1,
-            pre = 1,
-            post = 1;
-        while (r > -1 && l < nums.length) {
-            if (pre == 0) {
-                pre = 1;
+                r = nums.length - 1,
+                mid = 0;
+
+        while (l <= r) {
+            if (nums[l] < nums[r]) {
+                res = res < nums[l] ? res : nums[l];
             }
-            if (post == 0) {
-                post = 1;
+            mid = Math.floorDiv((l + r), 2);
+            res = res < nums[mid] ? res : nums[mid];
+            if (nums[mid] >= nums[l]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
-            pre = pre * nums[l];
-            l += 1;
-            post = post * nums[r];
-            r -= 1;
-            res = Math.max(res, Math.max(pre, post));
         }
         return res;
     }
--- Arrays/7findMinimumInRotatedSortedArray/findMinInRotatedSortedArr.py
+++ Arrays/7findMinimumInRotatedSortedArray/findMinInRotatedSortedArr.py
@@ -3,19 +3,34 @@
 # can able to use sliding window to search pair of decaresing value where it decreases we can find the minimum value
 class Solution:
     def findMin(self, nums: list[int]) -> int:
-        pre = 1
-        post = 1
-        r = len(nums) - 1
-        l = 0
         res = nums[0]
-        while r > -1 and l < len(nums):
-            if pre == 0:
-                pre = 1
-            if post == 0:
-                post = 1
-            pre *= nums[l]
-            l += 1
-            post *= nums[r]
-            r -= 1
-            res = max(pre, post, res)
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(nums[l], res)
+            mid = (l + r) // 2
+            res = min(res, nums[mid])
+            if nums[mid] >= nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1     
         return res
+class Solution2:
+    def findMin(self, nums: list[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        minimum = nums[0]
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] < minimum:
+                minimum = nums[mid]
+            if nums[mid] > nums[l]:
+                if nums[l] < minimum:
+                    minimum = nums[l]
+                l = mid + 1
+            else:
+                if nums[r] < minimum:
+                    minimum = nums[r]
+                r = mid - 1
+        return minimum
