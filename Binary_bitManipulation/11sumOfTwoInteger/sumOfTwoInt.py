7b778df20cd80c6cac3dd28bba7ce2002a18c767
Sugapriyan-P-K <sugapriyan2003@gmail.com>
2023-09-30 12:17:22 +0530

    update

--- Binary_bitManipulation/11sumOfTwoInteger/sumOfTwoInt.py
+++ Binary_bitManipulation/11sumOfTwoInteger/sumOfTwoInt.py
@@ -1,29 +1,4 @@
-# a =-14
-# b = 16
-# boo = False
-# if a < 0 and b >= 0:
-#     a = ~a
-#     a,b = b, a
-#     while (b != 0 ):
-#         pass
-# elif a >= 0 and b < 0:
-#     b -= 1
-#     print(a & b)
-# elif a < 0 and b < 0:
-#     a = -1 * a
-#     b = -1 * a
-#     boo = True
-# while True:
-#     if not b:
-#         print(a)
-#         break
-#     Sum = a ^ b
-#     carry = (a & b) << 1
-#     a = Sum
-#     b = carry
-# if boo:
-#     print(-1 * Sum)
-# print(Sum)
+
 class Solution:
     def addVal(self, a: int, b: int, mask: int) -> int:
         if b == 0:
